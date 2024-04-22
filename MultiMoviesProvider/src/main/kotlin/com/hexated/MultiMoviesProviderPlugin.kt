7bc4129d6fe96032c46af45503cd819d568f0f7d
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-04-22 17:19:49 +0530

    Update MultiMoviesProviderPlugin.kt

--- MultiMoviesProvider/src/main/kotlin/com/hexated/MultiMoviesProviderPlugin.kt
+++ MultiMoviesProvider/src/main/kotlin/com/hexated/MultiMoviesProviderPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class MultiMoviesProviderPlugin : Plugin() {
+class MultiMoviesProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(MultiMoviesProvider())
@@ -13,7 +13,5 @@ class MultiMoviesProviderPlugin : Plugin() {
     }
 
     private fun registerMultiMoviesExtractorAPI() {
-        val extractor = MultiMoviesExtractor()
-        registerExtractorAPI(extractor)
     }
 }
