7297a195c2c7f39c9d0de38c818243c74010f8f1
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-04-24 17:35:20 +0530

    Update MultiMoviesExtractor.kt

--- MultiMoviesProvider/src/main/kotlin/com/hexated/MultiMoviesExtractor.kt
+++ MultiMoviesProvider/src/main/kotlin/com/hexated/MultiMoviesExtractor.kt
@@ -1,10 +1,14 @@
 package com.hexated
 
+import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.network.WebViewResolver
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.INFER_TYPE
 import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.getAndUnpack
+import com.lagradost.cloudstream3.utils.getPacked
 
 open class MultiMoviesExtractor : ExtractorApi() {
     override var name = "MultiMovies"
@@ -31,11 +35,36 @@ open class MultiMoviesExtractor : ExtractorApi() {
             )
         return sources
     }
+}
 
-    suspend fun getExtractorLinks(url: String): List<ExtractorLink>? {
-        val referer = "$mainUrl/"
-        val streamWishLinks = getUrl(url, referer)
-        val vidHideLinks = getUrl(url.replace(mainUrl, "https://vidhide.com"), referer)
-        return (streamWishLinks ?: emptyList()) + (vidHideLinks ?: emptyList())
+class Fmhd : ExtractorApi() {
+    override val name = "Fmhd"
+    override var mainUrl = "https://animezia.cloud"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val response = app.get(url, referer = referer)
+        val script = if (!getPacked(response.text).isNullOrEmpty()) {
+            getAndUnpack(response.text)
+        } else {
+            response.document.selectFirst("script:containsData(sources:)")?.data()
+        }
+        val m3u8 = Regex("file:\\s*\"(.*?m3u8.*?)\"").find(script ?: return)?.groupValues?.get(1)
+
+        callback.invoke(
+            ExtractorLink(
+                this.name,
+                this.name,
+                m3u8 ?: return,
+                mainUrl,
+                Qualities.Unknown.value,
+                INFER_TYPE,
+            )
+        )
     }
 }
